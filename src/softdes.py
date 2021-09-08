b250085519d4d77a02a417f8d3305e05c9c18437
Gabriel Zezze <gabrielfz@al.insper.edu.br>
2021-09-08 17:43:42 -0300

    Fixed pylint warnings

--- src/softdes.py
+++ src/softdes.py
@@ -1,145 +1,143 @@
 # -*- coding: utf-8 -*-
+# pylint: disable=W0123
 """
 Created on Wed Jun 28 09:00:39 2017
 
 @author: rauli
 """
 
+from datetime import datetime
+import sqlite3
+import hashlib
 from flask import (
     Flask,
     request,
-    jsonify,
-    abort,
-    make_response,
-    session,
     render_template,
 )
 from flask_httpauth import HTTPBasicAuth
-from datetime import datetime
-import sqlite3
-import json
-import hashlib
+from util import converteData, lambda_handler
 
 DBNAME = "./quiz.db"
 
 
-def lambda_handler(event, context):
-    try:
-        import json
-        import numbers
-
-        def not_equals(first, second):
-            if isinstance(first, numbers.Number) and isinstance(second, numbers.Number):
-                return abs(first - second) > 1e-3
-            return first != second
-
-        # TODO implement
-        ndes = int(event["ndes"])
-        code = event["code"]
-        args = event["args"]
-        resp = event["resp"]
-        diag = event["diag"]
-        exec(code, locals())
-
-        test = []
-        for index, arg in enumerate(args):
-            if not "desafio{0}".format(ndes) in locals():
-                return "Nome da função inválido. Usar 'def desafio{0}(...)'".format(
-                    ndes
-                )
-
-            if not_equals(eval("desafio{0}(*arg)".format(ndes)), resp[index]):
-                test.append(diag[index])
-
-        return " ".join(test)
-    except:
-        return "Função inválida."
-
-
-def converteData(orig):
-    return (
-        orig[8:10]
-        + "/"
-        + orig[5:7]
-        + "/"
-        + orig[0:4]
-        + " "
-        + orig[11:13]
-        + ":"
-        + orig[14:16]
-        + ":"
-        + orig[17:]
-    )
-
-
-def getQuizes(user):
+def get_quizzes(user):
+    """
+    Get specified user quizzes release before today or if user is admin get all quizzes
+    Params:
+        user: User Id
+    Return:
+        List of quizzes
+    """
     conn = sqlite3.connect(DBNAME)
     cursor = conn.cursor()
-    if user == "admin" or user == "fabioja":
-        cursor.execute(
-            "SELECT id, numb from QUIZ".format(
-                datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            )
-        )
+    is_user_admin = user in ["admin", "fabioja"]
+    if is_user_admin:
+        cursor.execute("SELECT id, numb from QUIZ")
     else:
         cursor.execute(
             "SELECT id, numb from QUIZ where release < '{0}'".format(
                 datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             )
         )
-    info = [reg for reg in cursor.fetchall()]
+    info = list(cursor.fetchall())
     conn.close()
     return info
 
 
-# def getUserQuiz(userid, quizid):
-#     conn = sqlite3.connect(DBNAME)
-#     cursor = conn.cursor()
-#     cursor.execute(
-#         "SELECT sent,answer,result from USERQUIZ where userid = '{0}' and quizid = {1} order by sent desc".format(
-#             userid, quizid
-#         )
-#     )
-#     info = [reg for reg in cursor.fetchall()]
-#     conn.close()
-#     return info
-
-
-# def setUserQuiz(userid, quizid, sent, answer, result):
-#     conn = sqlite3.connect(DBNAME)
-#     cursor = conn.cursor()
-#     # print("insert into USERQUIZ(userid,quizid,sent,answer,result) values ('{0}',{1},'{2}','{3}','{4}');".format(userid, quizid, sent, answer, result))
-#     # cursor.execute("insert into USERQUIZ(userid,quizid,sent,answer,result) values ('{0}',{1},'{2}','{3}','{4}');".format(userid, quizid, sent, answer, result))
-#     cursor.execute(
-#         "insert into USERQUIZ(userid,quizid,sent,answer,result) values (?,?,?,?,?);",
-#         (userid, quizid, sent, answer, result),
-#     )
-#     #
-#     conn.commit()
-#     conn.close()
-
-
-def getQuiz(id, user):
+def get_user_quiz(userid, quizid):
+    """
+    Get specified quiz of specified user
+    """
     conn = sqlite3.connect(DBNAME)
     cursor = conn.cursor()
-    if user == "admin" or user == "fabioja":
+    cursor.execute(
+        """
+            SELECT
+                sent,
+                answer,
+                result
+            FROM
+               USERQUIZ
+            WHERE
+                userid = '{0}' and quizid = {1}
+            ORDER BY SENT DESC
+        """.format(
+            userid, quizid
+        )
+    )
+    info = list(cursor.fetchall())
+    conn.close()
+    return info
+
+
+def set_user_quiz(userid, quizid, sent, answer, result):
+    """
+    Submit user quiz answer
+    """
+    conn = sqlite3.connect(DBNAME)
+    cursor = conn.cursor()
+    cursor.execute(
+        "insert into USERQUIZ(userid,quizid,sent,answer,result) values (?,?,?,?,?);",
+        (userid, quizid, sent, answer, result),
+    )
+    conn.commit()
+    conn.close()
+
+
+def get_quiz(quiz_id, user):
+    """
+    If user is admin get all quizzes if not get quizzes available
+    """
+    conn = sqlite3.connect(DBNAME)
+    cursor = conn.cursor()
+    is_user_admin = user in ["admin", "fabioja"]
+    if is_user_admin:
         cursor.execute(
-            "SELECT id, release, expire, problem, tests, results, diagnosis, numb from QUIZ where id = {0}".format(
-                id
+            """
+                SELECT
+                    id,
+                    release,
+                    expire,
+                    problem,
+                    tests,
+                    results,
+                    diagnosis,
+                    numb
+                FROM
+                    QUIZ
+                WHERE id = {0}
+            """.format(
+                quiz_id
             )
         )
     else:
         cursor.execute(
-            "SELECT id, release, expire, problem, tests, results, diagnosis, numb from QUIZ where id = {0} and release < '{1}'".format(
-                id, datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            """
+                SELECT
+                    id,
+                    release,
+                    expire,
+                    problem,
+                    tests,
+                    results,
+                    diagnosis,
+                    numb
+                FROM
+                    QUIZ
+                WHERE id = {0} and release < '{1}'
+            """.format(
+                quiz_id, datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             )
         )
-    info = [reg for reg in cursor.fetchall()]
+    info = list(cursor.fetchall())
     conn.close()
     return info
 
 
-def setInfo(pwd, user):
+def set_info(pwd, user):
+    """
+    Set user info
+    """
     conn = sqlite3.connect(DBNAME)
     cursor = conn.cursor()
     cursor.execute("UPDATE USER set pass = ? where user = ?", (pwd, user))
@@ -147,17 +145,18 @@ def setInfo(pwd, user):
     conn.close()
 
 
-def getInfo(user):
+def get_info(user):
+    """
+    Get User info
+    """
     conn = sqlite3.connect(DBNAME)
     cursor = conn.cursor()
     cursor.execute("SELECT pass, type from USER where user = '{0}'".format(user))
-    print("SELECT pass, type from USER where user = '{0}'".format(user))
     info = [reg[0] for reg in cursor.fetchall()]
     conn.close()
     if len(info) == 0:
         return None
-    else:
-        return info[0]
+    return info[0]
 
 
 auth = HTTPBasicAuth()
@@ -169,22 +168,25 @@ app.secret_key = "A0Zr98j/3yX R~XHH!jmN]LWX/,?TX"
 @app.route("/", methods=["GET", "POST"])
 @auth.login_required
 def main():
+    """
+    Handle main page rendering depending on auth
+    """
     msg = ""
-    p = 1
-    challenges = getQuizes(auth.username())
+    phase = 1
+    challenges = get_quizzes(auth.username())
     sent = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     if request.method == "POST" and "ID" in request.args:
-        id = request.args.get("ID")
-        quiz = getQuiz(id, auth.username())
+        quiz_id = request.args.get("ID")
+        quiz = get_quiz(quiz_id, auth.username())
         if len(quiz) == 0:
             msg = "Boa tentativa, mas não vai dar certo!"
-            p = 2
+            phase = 2
             return render_template(
                 "index.html",
                 username=auth.username(),
                 challenges=challenges,
-                p=p,
+                p=phase,
                 msg=msg,
             )
 
@@ -192,24 +194,21 @@ def main():
         if sent > quiz[2]:
             msg = "Sorry... Prazo expirado!"
 
-        f = request.files["code"]
+        answer_file = request.files["code"]
         filename = "./upload/{0}-{1}.py".format(auth.username(), sent)
-        f.save(filename)
-        with open(filename, "r") as fp:
-            answer = fp.read()
+        answer_file.save(filename)
+        with open(filename, "r", encoding="UTF-8") as file_buffer:
+            answer = file_buffer.read()
 
         # lamb = boto3.client('lambda')
         args = {
-            "ndes": id,
+            "ndes": quiz_id,
             "code": answer,
             "args": eval(quiz[4]),
             "resp": eval(quiz[5]),
             "diag": eval(quiz[6]),
         }
 
-        # response = lamb.invoke(FunctionName="Teste", InvocationType='RequestResponse', Payload=json.dumps(args))
-        # feedback = response['Payload'].read()
-        # feedback = json.loads(feedback).replace('"','')
         feedback = lambda_handler(args, "")
 
         result = "Erro"
@@ -217,35 +216,39 @@ def main():
             feedback = "Sem erros."
             result = "OK!"
 
-        setUserQuiz(auth.username(), id, sent, feedback, result)
+        set_user_quiz(auth.username(), quiz_id, sent, feedback, result)
 
     if request.method == "GET":
         if "ID" in request.args:
-            id = request.args.get("ID")
+            quiz_id = request.args.get("ID")
         else:
-            id = 1
+            quiz_id = 1
 
     if len(challenges) == 0:
         msg = "Ainda não há desafios! Volte mais tarde."
-        p = 2
+        phase = 2
         return render_template(
-            "index.html", username=auth.username(), challenges=challenges, p=p, msg=msg
+            "index.html",
+            username=auth.username(),
+            challenges=challenges,
+            p=phase,
+            msg=msg,
         )
-    else:
-        quiz = getQuiz(id, auth.username())
 
-        if len(quiz) == 0:
-            msg = "Oops... Desafio invalido!"
-            p = 2
-            return render_template(
-                "index.html",
-                username=auth.username(),
-                challenges=challenges,
-                p=p,
-                msg=msg,
-            )
+    quiz = get_quiz(quiz_id, auth.username())
 
-        answers = getUserQuiz(auth.username(), id)
+    if len(quiz) == 0:
+        msg = "Oops... Desafio invalido!"
+        phase = 2
+        return render_template(
+            "index.html",
+            username=auth.username(),
+            challenges=challenges,
+            p=phase,
+            msg=msg,
+        )
+
+    answers = get_user_quiz(auth.username(), quiz_id)
 
     return render_template(
         "index.html",
@@ -254,7 +257,7 @@ def main():
         quiz=quiz[0],
         e=(sent > quiz[0][2]),
         answers=answers,
-        p=p,
+        p=phase,
         msg=msg,
         expi=converteData(quiz[0][2]),
     )
@@ -263,48 +266,60 @@ def main():
 @app.route("/pass", methods=["GET", "POST"])
 @auth.login_required
 def change():
+    """
+    Handle page rendering for password change
+    """
     if request.method == "POST":
         velha = request.form["old"]
         nova = request.form["new"]
         repet = request.form["again"]
 
-        p = 1
+        phase = 1
         msg = ""
         if nova != repet:
             msg = "As novas senhas nao batem"
-            p = 3
-        elif getInfo(auth.username()) != hashlib.md5(velha.encode()).hexdigest():
+            phase = 3
+        elif get_info(auth.username()) != hashlib.md5(velha.encode()).hexdigest():
             msg = "A senha antiga nao confere"
-            p = 3
+            phase = 3
         else:
-            setInfo(hashlib.md5(nova.encode()).hexdigest(), auth.username())
+            set_info(hashlib.md5(nova.encode()).hexdigest(), auth.username())
             msg = "Senha alterada com sucesso"
-            p = 3
+            phase = 3
     else:
         msg = ""
-        p = 3
+        phase = 3
 
     return render_template(
         "index.html",
         username=auth.username(),
-        challenges=getQuizes(auth.username()),
-        p=p,
+        challenges=get_quizzes(auth.username()),
+        p=phase,
         msg=msg,
     )
 
 
 @app.route("/logout")
 def logout():
+    """
+    Handle logout page rendering
+    """
     return render_template("index.html", p=2, msg="Logout com sucesso"), 401
 
 
 @auth.get_password
 def get_password(username):
-    return getInfo(username)
+    """
+    Get user password
+    """
+    return get_info(username)
 
 
 @auth.hash_password
 def hash_pw(password):
+    """
+    Handle password string hashing
+    """
     return hashlib.md5(password.encode()).hexdigest()
 
 
